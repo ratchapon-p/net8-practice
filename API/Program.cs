abb01b33399dff217dae89adcdc69414f87988c5
ratchapon-p <ratchapon.p01@gmail.com>
2024-11-26 23:13:11 +0700

    client side

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+//Configure http request pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
